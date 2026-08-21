d20d6969fd5b0885a79cc5518a6487823b30cb76
Ayberk Çağrı Dogan <ayberkcagridogan@gmail.com>
2026-08-21 15:49:31 +0200

    Refactored User Endpoint in Identity API

--- src/Services/Identity/EcoFleet.Identity.Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
+++ src/Services/Identity/EcoFleet.Identity.Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -1,7 +1,10 @@
 
 using EcoFleet.Identity.Application.Common.Authentication.Interfaces;
+using EcoFleet.Identity.Application.Common.Interfaces.Services;
 using EcoFleet.Identity.Application.Common.Persistence;
 using EcoFleet.Identity.Domain.Users;
+using EcoFleet.Shared.Kernel.Persistence.Interfaces;
+using EcoFleet.Shared.Kernel.Services.Interfaces;
 using MediatR;
 
 namespace EcoFleet.Identity.Application.Features.Users.Commands.CreateUser;
@@ -19,29 +22,34 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Creat
 {
     private readonly IUserRepo _userRepo;
     private readonly IPasswordHasher _passwordHasher;
-  //  private readonly IUnityOfWork _unityOfWork;
-   // private readonly ICurrentUserService _currentUserService;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly ICompanyGrpcClient _companyGrpcClient;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public CreateUserCommandHandler(IUserRepo userRepo, IPasswordHasher passwordHasher
-    //,   ICurrentUserService currentUserService, IUnityOfWork unityOfWork
-     )
+    public CreateUserCommandHandler(
+            IUserRepo userRepo, 
+            IPasswordHasher passwordHasher, 
+            ICurrentUserService currentUserService, 
+            IUnitOfWork unitOfWork,
+            ICompanyGrpcClient companyGrpcClient)
     {
         _userRepo = userRepo;
         _passwordHasher = passwordHasher;
-   //     _unityOfWork = unityOfWork;
-   //     _currentUserService = currentUserService;
+        _unitOfWork = unitOfWork;
+        _currentUserService = currentUserService;
+        _companyGrpcClient = companyGrpcClient;
     }
     public async Task<CreateUserResponse> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
         var passwordHash = _passwordHasher.HashPassword(request.Password);
 
-       // var companyId = _currentUserService.CompanyId 
-      //      ?? throw new UnauthorizedAccessException("Tenant/Company context is missing.");
+        var tensantId = _currentUserService.TenantId 
+            ?? throw new UnauthorizedAccessException("Tenant context is missing.");
 
-        var user = User.Create(request.FirstName, request.LastName, request.Email, passwordHash, Guid.NewGuid(), User.ResolveRole(request.Role));
+        var user = User.Create(request.FirstName, request.LastName, request.Email, passwordHash, tensantId, User.ResolveRole(request.Role));
 
         await _userRepo.AddAsync(user, cancellationToken);
-       // await _unityOfWork.SaveChangesAsync(cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
         
         return new CreateUserResponse(
             user.Id,
--- src/Services/Identity/EcoFleet.Identity.Application/Features/Users/Commands/CreateUser/CreateUserResponse.cs
+++ src/Services/Identity/EcoFleet.Identity.Application/Features/Users/Commands/CreateUser/CreateUserResponse.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace EcoFleet.Identity.Application.Features.Users.Commands.CreateUser
 {
     public record CreateUserResponse(Guid Id, string Email, string Role);
